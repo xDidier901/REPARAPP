fed9fbf07a031981b3ef7cb0a440ffa9276264e2
xDidier901 <xdidier901@gmail.com>
2018-04-16 03:49:49 -0700

    Creacion de modelos

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/didiervaldez/Desktop/PCFIXERS/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ reparapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ReparappConfig(AppConfig):
+    name = 'reparapp'
