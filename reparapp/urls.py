c1dcdc6a4dd188750f787be32216d03a8b9afd96
xDidier901 <xdidier901@gmail.com>
2018-04-16 21:12:10 -0700

    Se agregaron los modulos de Cliente y Equipo

--- reparapp/urls.py
+++ reparapp/urls.py
@@ -3,7 +3,24 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    #Reparaciones
     re_path(r'^reparacion/(?P<pk>\d+)/$', views.reparacion_detail, name='reparacion_detail'),
     re_path(r'^reparacion/new/$', views.reparacion_new, name='reparacion_new'),
     re_path(r'^reparacion/(?P<pk>\d+)/edit/$', views.reparacion_edit, name='reparacion_edit'),
+    re_path(r'^reparacion/(?P<pk>\d+)/remove/$', views.reparacion_remove, name='reparacion_remove'),
+
+    #Equipos
+    re_path(r'^equipo/$', views.equipo_list, name='equipo_list'),
+    re_path(r'^equipo/(?P<pk>\d+)/$', views.equipo_detail, name='equipo_detail'),
+    re_path(r'^equipo/new/$', views.equipo_new, name='equipo_new'),
+    re_path(r'^equipo/(?P<pk>\d+)/edit/$', views.equipo_edit, name='equipo_edit'),
+    re_path(r'^equipo/(?P<pk>\d+)/remove/$', views.equipo_remove, name='equipo_remove'),
+
+    #Clientes
+    re_path(r'^cliente/$', views.cliente_list, name='cliente_list'),
+    re_path(r'^cliente/(?P<pk>\d+)/$', views.cliente_detail, name='cliente_detail'),
+    re_path(r'^cliente/new/$', views.cliente_new, name='cliente_new'),
+    re_path(r'^cliente/(?P<pk>\d+)/edit/$', views.cliente_edit, name='cliente_edit'),
+    re_path(r'^cliente/(?P<pk>\d+)/remove/$', views.cliente_remove, name='cliente_remove'),
 ]
--- reparapp/views.py
+++ reparapp/views.py
@@ -4,11 +4,13 @@ from .models import Equipo
 from .models import Reparacion
 from django.shortcuts import redirect
 from .forms import ReparacionForm
+from .forms import EquipoForm
+from .forms import ClienteForm
 
 
 # Create your views here.
 
-
+########################## Vistas de Reparaciones ##########################
 def index(request):
     reparaciones = Reparacion.objects.order_by('fecha_llegada')
     return render(request, 'reparapp/index.html', {'reparaciones': reparaciones})
@@ -43,3 +45,87 @@ def reparacion_edit(request, pk):
         form = ReparacionForm(instance=reparacion)
     return render(request, 'reparapp/reparacion_edit.html', {'form': form})
 
+def reparacion_remove(request, pk):
+    reparacion = get_object_or_404(Reparacion, pk=pk)
+    reparacion.delete()
+    return redirect('index')
+
+
+########################## Vistas de Equipos ##########################
+
+def equipo_list(request):
+    equipos = Equipo.objects.order_by('pk')
+    return render(request, 'reparapp/equipo_list.html', {'equipos': equipos})
+
+def equipo_detail(request, pk):
+    equipo = get_object_or_404(Equipo, pk=pk)
+    return render(request, 'reparapp/equipo_detail.html', {'equipo': equipo})
+
+def equipo_new(request):
+    title = 'Agregar Equipo'
+    if request.method == "POST":
+        form = EquipoForm(request.POST)
+        if form.is_valid():
+            equipo = form.save(commit=False)
+            equipo.save()
+            return redirect('equipo_detail', pk=equipo.pk)
+    form = EquipoForm()
+    return render(request, 'reparapp/equipo_edit.html', {'form': form})
+
+def equipo_edit(request, pk):
+    title = 'Editar Equipo'
+    equipo = get_object_or_404(Equipo, pk=pk)
+    if request.method == "POST":
+        form = EquipoForm(request.POST, instance=equipo)
+        if form.is_valid():
+            equipo = form.save(commit=False)
+            equipo.save()
+            return redirect('equipo_detail', pk=equipo.pk)
+    else:
+        form = EquipoForm(instance=equipo)
+    return render(request, 'reparapp/equipo_edit.html', {'form': form})
+
+def equipo_remove(request, pk):
+    equipo = get_object_or_404(Equipo, pk=pk)
+    equipo.delete()
+    return redirect('equipo_list')
+
+
+########################## Vistas de Clientes ##########################
+
+def cliente_list(request):
+    clientes = Cliente.objects.order_by('pk')
+    return render(request, 'reparapp/cliente_list.html', {'clientes': clientes})
+
+def cliente_detail(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    return render(request, 'reparapp/cliente_detail.html', {'cliente': cliente})
+
+def cliente_new(request):
+    title = 'Agregar Cliente'
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            cliente = form.save(commit=False)
+            cliente.save()
+            return redirect('cliente_detail', pk=cliente.pk)
+    form = ClienteForm()
+    return render(request, 'reparapp/cliente_edit.html', {'form': form})
+
+def cliente_edit(request, pk):
+    title = 'Editar Cliente'
+    cliente = get_object_or_404(Cliente, pk=pk)
+    if request.method == "POST":
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            cliente = form.save(commit=False)
+            cliente.save()
+            return redirect('cliente_detail', pk=cliente.pk)
+    else:
+        form = ClienteForm(instance=cliente)
+    return render(request, 'reparapp/cliente_edit.html', {'form': form})
+
+def cliente_remove(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    cliente.delete()
+    return redirect('cliente_list')
